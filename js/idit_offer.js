c41e9c4a808f9fc9d575d78800dd5283b03792ae
Luvyjakk <slonik_zloy@mail.ru>
2023-10-10 21:09:24 +0300

    create new files

---
+++ js/idit_offer.js
@@ -0,0 +1,56 @@
+$ ('.name_edit').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg6').fadeIn(600);
+    
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg6').fadeOut(600);
+    
+});
+
+$ ('.categories_edit').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg7').fadeIn(600);
+    
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg7').fadeOut(600);
+    
+});
+
+$ ('.price_edit').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg8').fadeIn(600);
+    
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg8').fadeOut(600);
+   
+});
+
+$ ('.photo_edit').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg9').fadeIn(600);
+    
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg9').fadeOut(600);
+    
+});
+
+$ ('.description_edit').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg10').fadeIn(600);
+    
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg10').fadeOut(600);
+    
+});
+
+
---
+++ js/login.js
@@ -0,0 +1,7 @@
+$ ('.login_add_btn').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg11').fadeIn(1200);
+    $('.popup-bg11').fadeOut(500);
+    
+});
+
